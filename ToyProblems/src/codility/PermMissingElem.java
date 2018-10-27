6554795682e09e31837789a591f754a34b218727
Linda Wang <lindawang256@gmail.com>
2018-10-26 22:04:33 -0700

    Lesson 3 Perm Missing Element

---
+++ ToyProblems/src/codility/PermMissingElem.java
@@ -0,0 +1,44 @@
+/*
+ * An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A, returns the value of the missing element.
+
+For example, given array A such that:
+
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+the function should return 4, as it is the missing element.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+the elements of A are all distinct;
+each element of array A is an integer within the range [1..(N + 1)].
+ */
+package codility;
+import java.util.*;
+public class PermMissingElem {
+	    public int solution(int[] A) {
+	          // write your code in Java SE 8
+	        //sort A
+	        //loop through A from  i=0, i<A.length,
+	            //if A[i] != i+1 return i+1
+	        if (A.length==0) return 1;
+	        Arrays.sort(A);
+	        for (int i=0;i<A.length;i++){
+	            if (A[i]!=i+1){
+	                return i+1;
+	            }
+	        }
+	        return A[A.length-1]+1;
+	    }
+
+}
