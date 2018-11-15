935a7d9f7c095ae4701bd57110e8e58ee8d212bd
Linda Wang <lindawang256@gmail.com>
2018-11-15 08:32:10 -0800

    Implement dynamic programming

--- ToyProblems/src/dynamicProgram/Fibonacci.java
+++ ToyProblems/src/dynamicProgram/Fibonacci.java
@@ -7,9 +7,33 @@ public class Fibonacci {
 		return fib(n-1)+fib(n-2);
 	}
 	
+	public int fib2(int n){
+		if(n==1|| n==2) return 1;
+		return fib(n-1)+fib(n-2);
+		
+	}
+	public int fib3(int n){
+		int result=0;
+		int[] meno=new int[n+1];
+		result=fibDp(n,meno,result);
+		return result;
+	}
+	
+	public int fibDp(int n, int[] meno, int result){
+		if (meno[n]!=0) result =meno[n];
+		if (n==1 || n==2){
+			result=1;
+		}
+		else {
+			result=fib(n-1)+fib(n-2);
+			meno[n]=result;
+		}
+		return result;
+	}
+	
 	public static void main(String[] args){
 		Fibonacci f= new Fibonacci();
-		int res=f.fib(8);
+		int res=f.fib3(6);
 		System.out.println(res);
 		
 	}
